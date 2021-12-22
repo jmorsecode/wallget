168e6e5d87f5a73ee8a638740b511e2965a6feb9
jmorsecode <jmorsecode@github>
2021-12-21 21:36:23 -0500

    Overhaul poc code into more proper class usage

--- wallget.py
+++ wallget.py
@@ -1,29 +1,39 @@
+#!/bin/env python3
 import re
 import os
 import praw
 import urllib.request
 
 
-reddit = praw.Reddit("bot1",
-                     user_agent="wallget - Saved /r/wallpapers Downloader"
-                     )
-valid_ext = ["png", "jpg", "jpeg", "webp"]
-saved = reddit.user.me().saved(limit=50)
+class Wallget:
+    def __init__(self, prawbot):
+        self.valid_ext = ["png", "jpg", "jpeg", "webp"]
+        self.prawbot = prawbot
+        self.saved = prawbot.user.me().saved(limit=50)
 
-print(f'Retreiving saved wallpapers for: {reddit.user.me()}')
+    def download(self):
+        print(f'Retreiving saved wallpapers for: {self.prawbot.user.me()}')
 
-for link in saved:
-    if link.subreddit == 'wallpapers':
-        sanitized_title = re.sub('[\W_]+', '', link.title)
-        extension = link.url.split('.')[-1].lower()
-        if extension not in valid_ext:
-            continue
-        filename = f'{sanitized_title}.{extension}'
-        if os.path.exists(filename):
-            continue
-        print(f'{link.title} : {link.url}')
+        downloads = 0
+        for link in self.saved:
+            if link.subreddit == 'wallpapers':
+                sanitized_title = re.sub('[\W_]+', '', link.title)
+                extension = link.url.split('.')[-1].lower()
+                filename = f'{sanitized_title}.{extension}'
+                if extension not in self.valid_ext:
+                    continue
+                if os.path.exists(filename):
+                    continue
+                print(f'{link.title} : {link.url}')
+                urllib.request.urlretrieve(link.url, filename)
+                downloads += 1
 
-        urllib.request.urlretrieve(link.url, filename)
+        if downloads:
+            print(f"{downloads} download(s) completed.")
 
-print("Download(s) completed.")
-
+if __name__ == '__main__':
+    prawbot1 = praw.Reddit("bot1",
+                         user_agent="wallget - Saved /r/wallpapers Downloader"
+                  )
+    downloader = Wallget(prawbot1)
+    downloader.download()
